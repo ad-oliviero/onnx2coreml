e95f0afb0e736c0ab8f9f45b00d27fc74e907ea3
Adriano Oliviero <adrianoliviero23@gmail.com>
2025-11-11 17:44:10 +0100

    Rewritten to be more readable and scalable, fixed some stuff

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+import shutil
 import subprocess as sp
 
 import coremltools as ct
@@ -5,75 +7,96 @@ import onnx
 from onnxsim import simplify
 
 
-def convert(model_name: [str, str, str]):
-    model_name[1] = model_name[0].replace(".onnx", "-simplified.onnx")
-    model = onnx.load(model_name[0])
+class MlModel(object):
+    def __init__(
+        self, file, author, license, short_description, version, input_desc, output_desc
+    ) -> None:
+        self.file = file
+        self.author = author
+        self.license = license
+        self.short_description = short_description
+        self.version = version
+        self.input_desc = input_desc
+        self.output_desc = output_desc
+        self.simp_file = file.replace(".onnx", "-simplified.onnx")
+        self.coreml_file = self.simp_file.replace(".onnx", ".mlpackage")
+        self.saved_model_dir = "saved_model"
 
-    model_simp, check = simplify(
-        model,
-        input_shapes={"images": [1, 3, 768, 768]},
-        dynamic_input_shape=False,
-        skip_fuse_bn=True,
-    )
+    def convert(self):
+        if os.path.exists(self.saved_model_dir):
+            shutil.rmtree(self.saved_model_dir)
+        model = onnx.load(self.file)
+        model_simp, check = simplify(
+            model,
+            overwrite_input_shapes={"images": [1, 3, 768, 768]},
+            dynamic_input_shape=False,
+            skip_fuse_bn=True,
+            perform_optimization=True,
+        )
 
-    if check:
-        onnx.save(model_simp, model_name[1])
-        print(f"Saved {model_name[0]} to {model_name[1]}")
-    else:
-        raise
+        if check:
+            onnx.save(model_simp, self.simp_file)
+            print(f"Simplified {self.file} to {self.simp_file}")
+        else:
+            raise RuntimeError(f"Failed to simplify {self.file}")
 
-    sp.run(f"onnx2tf -i {model_name[1]}", shell=True, check=True)
-    model_name[2] = model_name[0].replace(".onnx", ".mlpackage")
-    model = ct.convert(
-        "saved_model",
-        source="tensorflow",
-        convert_to="mlprogram",
-        minimum_deployment_target=ct.target.iOS18,
-        compute_precision=ct.precision.FLOAT16,
-    )
+        sp.run(
+            f"onnx2tf -osd -nuo -dgc -v info -i {self.simp_file} -o {self.saved_model_dir}",
+            shell=True,
+            check=True,
+        )
+        model = ct.convert(
+            self.saved_model_dir,
+            source="tensorflow",
+            convert_to="mlprogram",
+            minimum_deployment_target=ct.target.iOS18,
+            compute_precision=ct.precision.FLOAT16,
+        )
 
-    spec = model.get_spec()
-    print("\nInputs:")
-    for input_desc in spec.description.input:
-        print(f"  Name: {input_desc.name}")
-        print(f"  Type: {input_desc.type}")
+        spec = model.get_spec()
+        # print("\nInputs:")
+        # for input_desc in spec.description.input:
+        #     print(f"  Name: {input_desc.name}")
+        #     print(f"  Type: {input_desc.type}")
 
-    print("\nOutputs:")
-    for output_desc in spec.description.output:
-        print(f"  Name: {output_desc.name}")
-        print(f"  Type: {output_desc.type}")
+        # print("\nOutputs:")
+        # for output_desc in spec.description.output:
+        #     print(f"  Name: {output_desc.name}")
+        #     print(f"  Type: {output_desc.type}")
 
-    model.author = "Pix2Text-MFD"
-    model.license = "Apache 2.0"
-    model.short_description = "Mathematical Formula Detection Model"
-    model.version = "1.5"
+        model.author = self.author
+        model.license = self.license
+        model.short_description = self.short_description
+        model.version = self.version
 
-    output_name = spec.description.output[0].name
-    input_name = spec.description.input[0].name
+        output_name = spec.description.output[0].name
+        input_name = spec.description.input[0].name
 
-    print(f"\nActual input name: {input_name}")
-    print(f"Actual output name: {output_name}")
+        model.input_description[input_name] = self.output_desc
+        model.output_description[output_name] = self.output_desc
 
-    model.input_description[input_name] = (
-        "Input image (768x768 RGB), values normalized to [0, 1]"
-    )
-    model.output_description[output_name] = (
-        "Detection output (1, 6, 12096) - bounding boxes and scores"
-    )
+        self.coreml_file = self.coreml_file.replace("-simplified", "")
+        model.save(self.coreml_file)
+        print(f"Saved {self.file} to {self.coreml_file}")
 
-    model.save(model_name[2])
-
-    print(f"Saved as: {model_name[2]}")
-    sp.run("rm -r saved_model", shell=True, check=True)
+        shutil.rmtree(self.saved_model_dir)
 
 
 if __name__ == "__main__":
     # Right now this is how you set the models to convert,
     # in future (if I keep to maintain this project) it will be easier
-    model_names = [
-        ["pix2text-mfd-1.5/pix2text-mfd-1.5.onnx", "", ""],
+    src_models = [
+        MlModel(
+            "pix2text-mfd-1.5/pix2text-mfd-1.5.onnx",
+            "Pix2Text-MFD",
+            "Apache 2.0",
+            "Mathematical Formula Detection Model",
+            "1.5",
+            "Input image (768x768 RGB), values normalized to [0, 1]",
+            "Detection output (1, 6, 12096) - bounding boxes and scores",
+        )
         # ["", "", ""],
         # ["", "", ""],
     ]
-    for nm in model_names:
-        convert(nm)
+    for model in src_models:
+        model.convert()
